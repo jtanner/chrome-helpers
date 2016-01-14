1bef8428c830dd65b4e11880df158f09257369bc
Joe Tanner <joe.j.tanner@gmail.com>
2016-01-13 17:16:31 -0700

    Add centerFocusedElement command; remove coffeescript

--- console.js
+++ console.js
@@ -1,11 +1,36 @@
-function customConsole() {
-  window.json = function(o) {
-    return JSON.stringify(o, null, '  ')
-  }
-}
+(function() {
 
-// inject custom console functions
-var script = document.createElement('script'),
-    code   = document.createTextNode('(' + customConsole + ')();');
-script.appendChild(code);
-(document.body || document.head || document.documentElement).appendChild(script);
+  // listen for commands
+  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (typeof commands[request.command] === "function") {
+      commands[request.command]()
+    }
+  });
+
+  var commands = {
+    centerFocusedElement: function () { scrollIntoViewCenter(document.activeElement.parentNode) }
+  };
+
+  function scrollIntoViewCenter (e) {
+    window.scrollTo(0, documentOffsetTop(e) - (window.innerHeight / 2))
+  }
+
+  function documentOffsetTop (e) {
+    return e.offsetTop + (e.offsetParent ? documentOffsetTop(e.offsetParent) : 0)
+  }
+
+  /* --------------------------------------------- */
+
+  function customConsole() {
+    window.json = function(o) {
+      return JSON.stringify(o, null, '  ')
+    }
+  }
+
+  // inject custom console functions
+  var script = document.createElement('script'),
+  code   = document.createTextNode('(' + customConsole + ')();');
+  script.appendChild(code);
+  (document.body || document.head || document.documentElement).appendChild(script);
+
+}).call(this);
